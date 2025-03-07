a16274ac3a5cf510a861ca8146b375f68c1229a6
Abdul Rafay <rajpootrafay96@gmail.com>
2025-03-07 10:19:51 +0500

    java script first Day assignment will done 4 will created and one will chat gpt now uplaoded

---
+++ FirstDay/Assignmentone.js
@@ -0,0 +1,69 @@
+// problem one solve in javascript  
+// problem one is solve in javascript
+var number = 65
+if (number <= 18) {
+    console.log('You got 20% discount')
+}
+else if (number = 18 && number <=65) {
+    console.log("Normal ticket applies")
+
+}
+else {
+    console.log("You get 30% Senior Discount");
+}
+
+
+// problem second is Start 
+
+var data = 13
+var num = 15
+
+data * num
+console.log(13 * 15)
+
+
+
+// problem third is tart now 
+
+const objecteddata = ({
+    name: "Ali",
+    price: 7999,
+    inStock: true,
+
+})
+
+console.log( objecteddata)
+
+
+// problem fourth 
+
+const guestList = ["rafay", "Ali", "Ahmed", "Bilal", "Saif"]
+
+function CheckList(name) {
+    if(guestList) {
+        console.log(`Welcome to the Party" ${name}`)
+    }
+    else {
+        console.log("Sorry, the party is full")
+    }
+}
+
+CheckList("Ali")
+CheckList(" rafay")
+
+// problem five is start now
+
+
+const weatherData = {
+    "date": "3-07-2025",
+    temperature: "22°C",
+    condition: "Sunny",
+    huumidty: "55%"
+}
+
+console.log(weatherData)
+console.log(`weatherData: ${weatherData.date}`)
+console.log(`weatherData: ${weatherData.temperature}`)
+console.log(`weatherData: ${weatherData.condition}`)
+console.log(`weatherData: ${weatherData.huumidty}`)
+
